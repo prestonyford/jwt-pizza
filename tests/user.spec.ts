0f8c4f3554a160361c2b27736a44c14dc2da4fb6
Preston Ford <preston.y.ford@gmail.com>
2026-02-16 18:52:34 -0700

    mock backend in user tests

--- tests/user.spec.ts
+++ tests/user.spec.ts
@@ -1,6 +1,73 @@
 import { expect, test } from "playwright-test-coverage";
+import { Page } from "playwright/test";
+import { Role, User } from "../src/service/pizzaService";
+
+async function basicInit(page: Page) {
+	let loggedInUser: User | undefined;
+
+	await page.route("*/**/api/auth", async (route) => {
+		const method = route.request().method();
+		if (method === "POST" || method === "PUT") {
+			const req = route.request().postDataJSON();
+			loggedInUser = Object.assign(loggedInUser ?? {}, {
+				...req,
+				id: 306,
+				roles: [{ role: Role.Diner }],
+			}) as User;
+			const res = {
+				"user": {
+					"name": loggedInUser.name,
+					"email": loggedInUser.email,
+					"roles": loggedInUser.roles,
+					"id": loggedInUser.id
+				},
+				"token": "abcdef",
+			};
+			await route.fulfill({ json: res });
+		} else if (method === "DELETE") {
+			route.fulfill({ json: { message: "logout successful" } });
+		}
+	});
+	await page.route("*/**/api/order", async (route) => {
+		const method = route.request().method();
+		if (method === "GET") {
+			if (!loggedInUser) {
+				await route.fulfill({
+					status: 401,
+					json: { error: "Unauthorized" },
+				});
+				return;
+			}
+			const res = {
+				"dinerId": loggedInUser.id,
+				"orders": [],
+				"page": 1,
+			};
+			await route.fulfill({ json: res });
+		}
+	});
+	await page.route("*/**/api/user/*", async (route) => {
+		const method = route.request().method();
+		if (method === "PUT") {
+			if (!loggedInUser) {
+				await route.fulfill({
+					status: 401,
+					json: { error: "Unauthorized" },
+				});
+				return;
+			}
+			const req = route.request().postDataJSON();
+			loggedInUser.name = req.name;
+			loggedInUser.email = req.email;
+			const res = { user: { ...req } };
+			await route.fulfill({ json: res });
+		}
+	});
+}
 
 test("updateUser", async ({ page }) => {
+	basicInit(page);
+
 	const email = `user${Math.floor(Math.random() * 10000)}@jwt.com`;
 	await page.goto("/");
 	await page.getByRole("link", { name: "Register" }).click();
