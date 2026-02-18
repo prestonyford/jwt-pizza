49ef8efaddd3b53a16aa780081f2dc154c7b03c6
Preston Ford <preston.y.ford@gmail.com>
2026-02-17 22:56:22 -0700

    add admin login delete user test

--- tests/admin.spec.ts
+++ tests/admin.spec.ts
@@ -105,48 +105,59 @@ async function basicInit(page: Page) {
 	});
 
 	await page.route("*/**/api/user?*", async (route) => {
-		expect(route.request().method()).toBe("GET");
-		await route.fulfill({
-			json: {
-				users: [
-					{
-						"id": 1,
-						"name": "常用名字",
-						"email": "a@jwt.com",
-						"roles": [
-							{
-								"role": "admin",
-							},
-						],
-					},
-					{
-						"id": 2,
-						"name": "pizza diner",
-						"email": "d@jwt.com",
-						"roles": [
-							{
-								"role": "diner",
-							},
-						],
-					},
-					{
-						"id": 3,
-						"name": "pizza franchisee",
-						"email": "f@jwt.com",
-						"roles": [
-							{
-								"role": "diner",
-							},
-							{
-								"role": "franchisee",
-								"objectId": 1,
-							},
-						],
-					},
-				],
-				more: false,
-			},
-		});
+		const method = route.request().method();
+		if (method === "GET") {
+			await route.fulfill({
+				json: {
+					users: [
+						{
+							"id": 1,
+							"name": "常用名字",
+							"email": "a@jwt.com",
+							"roles": [
+								{
+									"role": "admin",
+								},
+							],
+						},
+						{
+							"id": 2,
+							"name": "pizza diner",
+							"email": "d@jwt.com",
+							"roles": [
+								{
+									"role": "diner",
+								},
+							],
+						},
+						{
+							"id": 3,
+							"name": "pizza franchisee",
+							"email": "f@jwt.com",
+							"roles": [
+								{
+									"role": "diner",
+								},
+								{
+									"role": "franchisee",
+									"objectId": 1,
+								},
+							],
+						},
+					],
+					more: false,
+				},
+			});
+		}
+	});
+
+	await page.route("*/**/api/user/*", async (route) => {
+		const method = route.request().method();
+		if (method === "DELETE") {
+			await route.fulfill({
+				json: { message: 'ok' }
+			});
+		}
 	});
 
 	await page.goto("/");
@@ -221,7 +232,6 @@ test("login admin close franchise", async ({ page }) => {
 	await expect(page.locator("h2")).toContainText("Mama Ricci's kitchen");
 });
 
-
 test("login admin view users", async ({ page }) => {
 	await basicInit(page);
 
@@ -246,8 +256,49 @@ test("login admin view users", async ({ page }) => {
 	await page.getByRole("link", { name: "Admin" }).click();
 	await expect(page.locator("h2")).toContainText("Mama Ricci's kitchen");
 
-	await expect(page.getByRole('cell', { name: '常用名字' })).toBeVisible();
-	await expect(page.getByRole('cell', { name: 'a@jwt.com' })).toBeVisible();
-	await expect(page.getByRole('cell', { name: 'admin' })).toBeVisible();
-	await expect(page.getByRole('row', { name: '常用名字 a@jwt.com admin Delete' }).getByRole('button')).toBeVisible();
+	await expect(page.getByRole("cell", { name: "常用名字" })).toBeVisible();
+	await expect(page.getByRole("cell", { name: "a@jwt.com" })).toBeVisible();
+	await expect(page.getByRole("cell", { name: "admin" })).toBeVisible();
+	await expect(
+		page.getByRole("row", { name: "常用名字 a@jwt.com admin Delete" })
+			.getByRole("button"),
+	).toBeVisible();
+});
+
+test("login admin delete user", async ({ page }) => {
+	await basicInit(page);
+
+	await page.getByLabel("Global").getByRole("link", { name: "Franchise" })
+		.click();
+	await expect(page.getByRole("main")).toContainText(
+		"So you want a piece of the pie?",
+	);
+
+	await expect(page.getByRole("alert")).toContainText(
+		"If you are already a franchisee, pleaseloginusing your franchise account",
+	);
+	await page.getByRole("link", { name: "login", exact: true }).click();
+	await page.getByRole("textbox", { name: "Email address" }).click();
+	await page.getByRole("textbox", { name: "Email address" }).fill(
+		"a@jwt.com",
+	);
+	await page.getByRole("textbox", { name: "Email address" }).press("Tab");
+	await page.getByRole("textbox", { name: "Password" }).fill("admin");
+	await page.getByRole("button", { name: "Login" }).click();
+
+	await page.getByRole("link", { name: "Admin" }).click();
+
+	await page.getByRole("row", { name: "常用名字 a@jwt.com admin Delete" })
+		.getByRole("button").click();
+
+	await expect(page.getByRole("heading")).toContainText(
+		"Are you sure?",
+	);
+	await expect(page.getByRole("main")).toContainText(
+		"Are you sure you want to delete the user 常用名字?",
+	);
+	await expect(page.getByRole("button", { name: "Delete" })).toBeVisible();
+	await page.getByRole("button", { name: "Delete" }).click();
+
+	await expect(page.locator("h2")).toContainText("Mama Ricci's kitchen");
 });
