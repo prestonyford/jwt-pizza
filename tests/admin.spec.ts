60746b73e7ab1a1b475a3a1faa3297493917a6d2
Preston Ford <preston.y.ford@gmail.com>
2026-02-17 17:42:47 -0700

    Add admin dashboard users table test

--- tests/admin.spec.ts
+++ tests/admin.spec.ts
@@ -57,9 +57,11 @@ async function basicInit(page: Page) {
 			];
 			await route.fulfill({ json: res });
 		} else if (route.request().method() === "DELETE") {
-			route.fulfill({ json: {
-				message: "store deleted"
-			}});
+			route.fulfill({
+				json: {
+					message: "store deleted",
+				},
+			});
 		}
 	});
 
@@ -100,6 +102,51 @@ async function basicInit(page: Page) {
 	await page.route("*/**/api/user/me", async (route) => {
 		expect(route.request().method()).toBe("GET");
 		await route.fulfill({ json: loggedInUser });
+	});
+
+	await page.route("*/**/api/user?*", async (route) => {
+		expect(route.request().method()).toBe("GET");
+		await route.fulfill({
+			json: {
+				users: [
+					{
+						"id": 1,
+						"name": "常用名字",
+						"email": "a@jwt.com",
+						"roles": [
+							{
+								"role": "admin",
+							},
+						],
+					},
+					{
+						"id": 2,
+						"name": "pizza diner",
+						"email": "d@jwt.com",
+						"roles": [
+							{
+								"role": "diner",
+							},
+						],
+					},
+					{
+						"id": 3,
+						"name": "pizza franchisee",
+						"email": "f@jwt.com",
+						"roles": [
+							{
+								"role": "diner",
+							},
+							{
+								"role": "franchisee",
+								"objectId": 1,
+							},
+						],
+					},
+				],
+				more: false,
+			},
+		});
 	});
 
 	await page.goto("/");
@@ -158,8 +205,10 @@ test("login admin close franchise", async ({ page }) => {
 
 	await page.getByRole("link", { name: "Admin" }).click();
 
-	await page.getByRole('row', { name: '0edfkqzkk8 Close' }).getByRole('button').click();
-		
+	await page.getByRole("row", { name: "0edfkqzkk8 Close" }).getByRole(
+		"button",
+	).click();
+
 	await expect(page.getByRole("heading")).toContainText(
 		"Sorry to see you go",
 	);
@@ -168,6 +217,37 @@ test("login admin close franchise", async ({ page }) => {
 	);
 	await expect(page.getByRole("button", { name: "Close" })).toBeVisible();
 	await page.getByRole("button", { name: "Close" }).click();
-	
+
 	await expect(page.locator("h2")).toContainText("Mama Ricci's kitchen");
+});
+
+
+test("login admin view users", async ({ page }) => {
+	await basicInit(page);
+
+	await page.getByLabel("Global").getByRole("link", { name: "Franchise" })
+		.click();
+	await expect(page.getByRole("main")).toContainText(
+		"So you want a piece of the pie?",
+	);
+
+	await expect(page.getByRole("alert")).toContainText(
+		"If you are already a franchisee, pleaseloginusing your franchise account",
+	);
+	await page.getByRole("link", { name: "login", exact: true }).click();
+	await page.getByRole("textbox", { name: "Email address" }).click();
+	await page.getByRole("textbox", { name: "Email address" }).fill(
+		"a@jwt.com",
+	);
+	await page.getByRole("textbox", { name: "Email address" }).press("Tab");
+	await page.getByRole("textbox", { name: "Password" }).fill("admin");
+	await page.getByRole("button", { name: "Login" }).click();
+
+	await page.getByRole("link", { name: "Admin" }).click();
+	await expect(page.locator("h2")).toContainText("Mama Ricci's kitchen");
+
+	await expect(page.getByRole('cell', { name: '常用名字' })).toBeVisible();
+	await expect(page.getByRole('cell', { name: 'a@jwt.com' })).toBeVisible();
+	await expect(page.getByRole('cell', { name: 'admin' })).toBeVisible();
+	await expect(page.getByRole('row', { name: '常用名字 a@jwt.com admin Delete' }).getByRole('button')).toBeVisible();
 });
